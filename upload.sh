2e9cc0c007f85e0c7f48c9667058c3949315b833
cohenaj194 <cohenaj194@gmail.com>
2018-04-20 10:44:04 -0700

    upload update

--- upload.sh
+++ upload.sh
@@ -13,6 +13,11 @@ aws s3 ls s3://storage-bucket-nssa620
 aws s3 cp ./test.txt s3://storage-bucket-nssa620
 aws s3 ls s3://storage-bucket-nssa620
 
+# this will not work from sources outside of the bastion
+aws s3 ls s3://storage-bucket-nssa620
+aws s3 cp ~/Downloads/test.txt s3://storage-bucket-nssa620
+
 # s3 download
 aws s3 cp s3://storage-bucket-nssa620 . --recursive
 scp ubuntu@$EIP:~/test.txt .
+
